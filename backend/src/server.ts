accbac47ecf51513da29200e40da9cc4a20a5134
erikalbuquerque <eriksilv.77@gmail.com>
2021-01-03 00:11:49 -0300

    initialize frontEnd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,25 @@
 import express from "express";
-import cors from "cors";
 import "./database/connection";
+
+import cors from "cors";
 import path from "path";
+
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 import routes from "./routes";
 
 const app = express();
+const http = createServer(app);
+const io = new Server(http);
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads/", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333, () => console.log("Server running!"));
+io.on("connection", (socket: Socket) => {
+  console.log("↱ : client connected!");
+});
+
+http.listen(3333, () => console.log("⇌ : server running!"));
